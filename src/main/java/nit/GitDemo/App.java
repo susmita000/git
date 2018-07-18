430433e1ccfb2d45232d1bbbe168f35362e9d70b
susmita000 <susmitapanigrahi6@gmail.com>
2018-07-19 00:41:37 +0530

    one more class added

--- src/main/java/nit/GitDemo/App.java
+++ src/main/java/nit/GitDemo/App.java
@@ -8,6 +8,7 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("data added");
+		System.out.println("more data got added");
 	}
     public static void main( String[] args )
     {
---
+++ src/main/java/nit/GitDemo/Calculate.java
@@ -0,0 +1,6 @@
+package nit.GitDemo;
+
+public class Calculate {
+	private String data_Type="javatechie";
+
+}
