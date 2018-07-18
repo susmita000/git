b19756bd9f60d5bfb915db44217a1d9a12254472
susmita000 <susmitapanigrahi6@gmail.com>
2018-07-18 18:32:06 +0530

    added git demo proj to local repo

---
+++ src/main/java/nit/GitDemo/App.java
@@ -0,0 +1,16 @@
+package nit.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data) {
+		System.out.println("data added");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
